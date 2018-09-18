a9e39ac3b3829d5b01305b4a882c2b611fb32fee
philiplackmaker <philip@potatolondon.com>
2018-09-18 11:37:00 +0100

    i

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,43 +1,7 @@
-const path = require('path')
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
 
-
-
-exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions
-
-  return graphql(`
-    {
-    allMarkdownRemark(limit: 1000) {
-      edges {
-        node {
-          id
-          frontmatter {
-            path
-            templateKey 
-          }
-        }
-      }
-    }
-  }
-  `).then(result => {
-    if (result.errors) {
-      result.errors.forEach(e => console.error(e.toString()))
-      return Promise.reject(result.errors)
-    }
-
-    const posts = result.data.allMarkdownRemark.edges
-
-    posts.forEach(edge => {
-      const id = edge.node.id
-      createPage({
-        path: edge.node.frontmatter.path,
-        component: path.resolve(
-          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
-        ),
-        // additional data can be passed via context
-        context: {
-          id,
-        },
-      })
-    })
-  })}
+// You can delete this file if you're not using it
