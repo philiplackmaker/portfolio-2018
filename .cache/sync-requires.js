1a0c6e760cec882f31e58045069b876560ab7840
philiplackmaker <philip@potatolondon.com>
2018-09-19 09:41:17 +0100

    i :wq

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/philip/development/portfolio-2018/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/philip/development/portfolio-2018/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/philip/development/portfolio-2018/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/Users/philip/development/portfolio-2018/src/pages/page-2.js"))
+}
+
