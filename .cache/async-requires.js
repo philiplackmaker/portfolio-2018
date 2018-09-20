413181fb832b464fc74e5dc75767d7075fe49cd6
philiplackmaker <philip@potatolondon.com>
2018-09-20 18:14:31 +0100

    adding 404.

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -6,6 +6,7 @@ exports.components = {
   "component---src-templates-thinking-js": () => import("/Users/philip/development/portfolio-2018/src/templates/thinking.js" /* webpackChunkName: "component---src-templates-thinking-js" */),
   "component---src-templates-about-js": () => import("/Users/philip/development/portfolio-2018/src/templates/about.js" /* webpackChunkName: "component---src-templates-about-js" */),
   "component---cache-dev-404-page-js": () => import("/Users/philip/development/portfolio-2018/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("/Users/philip/development/portfolio-2018/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-index-js": () => import("/Users/philip/development/portfolio-2018/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
 }
 
