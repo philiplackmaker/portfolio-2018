8cd437115dab4e4e0742fd08e96613b547369ae9
philiplackmaker <philip@potatolondon.com>
2018-10-02 13:25:14 +0100

    Adding test form

--- src/templates/about.js
+++ src/templates/about.js
@@ -21,6 +21,21 @@ export default function Template({ data }) {
         <h6>{about.frontmatter.title_test}</h6>
         <Img fluid={about.frontmatter.cover_image.childImageSharp.fluid} />
       </Container>
+      <form name="contact" netlify>
+        <p>
+          <label>
+            Name <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Email <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
       <FooterEverything />
     </div>
   );
