511a63db8fcecb0c3dad574752dc40096679de5c
philiplackmaker <philip@potatolondon.com>
2018-10-09 14:01:02 +0100

    Adding FA

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,26 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-71700644-2",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Enables Google Optimize using your container Id
+        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "https://sharp-poincare-20a9cf.netlify.com/"
+      }
+    },
+    {
       resolve: "gatsby-plugin-intercom-spa",
       options: {
         app_id: "d7vu7om5"
